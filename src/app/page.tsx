986b3de8821236a1989783fe42b89e9b20d9130c
Ronald Gustavo <Ronald@dbo.id>
2025-08-07 10:41:29 +0700

    fix: build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 
 import React from 'react';
 import Pokedex from '../components/Pokedex';
-import { getPokemons } from '../services/pokemon.service';
 
 export const dynamic = 'force-dynamic';
 
 export default async function HomePage() {
-  const pokemons = await getPokemons();
-  return <Pokedex pokemons={pokemons} />;
+  return <Pokedex />;
 }
